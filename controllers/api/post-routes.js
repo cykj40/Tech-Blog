d0d4df7c41c060d7a28c0a9085dbd53312bdd94f
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-11-13 11:18:24 -0500

    Update post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
-// const sequelize = require('../../config/connection');
+const { User, Post, Comment } = require('../../Models');
+const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
 // get all users
